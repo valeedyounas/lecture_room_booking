9c9ebb439e086a28e24bb259412ae1c0b978784c
Valeed <valeedyounas>
2020-06-27 15:18:02 +0500

    Accept client implemented

--- src/server/AcceptClient.java
+++ src/server/AcceptClient.java
@@ -6,32 +6,23 @@ public class AcceptClient extends Thread {
     ClientCommunicator any;
     Socket connectSocket;
     public void run() {
-
-        while (true)
-        {
-            try {
-                System.out.println("Waiting for client to connect!");
-                connectSocket = any.acceptFrom_OtherClient();
-                System.out.println("client Connected" + connectSocket.getLocalPort());
-            }
-            catch (Exception e) { e.printStackTrace(); }
-
-            if (connectSocket!=null)
+        try {
+            while(true)
             {
-
-                DisplayReceiveThread t1 = new DisplayReceiveThread(connectSocket,any,msgs,list);
-                t1.start();
+                connectSocket =any.acceptFrom_OtherClient();
+                System.out.println("Client connected"+ connectSocket.getPort());
             }
-
         }
+        catch (Exception e) {
+            e.printStackTrace(); }
+
     }
-    public AcceptClientThread(Client_communicator x, ObservableList<String> h,ArrayList<ActiveUser> l) {
+    public AcceptClient(ClientCommunicator x) {
         // TODO Auto-generated constructor stub
         any = x;
-        connectSocket = null;
-        msgs = h;
-        list = l;
+
     }
+
 
     public Socket getConnectSocket() {
         return connectSocket;
