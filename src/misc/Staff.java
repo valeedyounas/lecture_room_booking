9b0fddbcc33cfeffd33a720cf94f107edfa03872
Shaheer Hassan <shaheerhassan@live.com>
2020-06-27 15:07:57 +0500

    added staff class into misc

---
+++ src/misc/Staff.java
@@ -0,0 +1,34 @@
+package misc;
+
+public class Staff {
+    String name;
+    int id;
+    String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
