d6f2c99e1f9dbf13db46b0e6ef6168c7312f0928
Shaheer Hassan <shaheerhassan@live.com>
2020-06-27 17:18:14 +0500

    verification implemented

--- src/misc/Staff.java
+++ src/misc/Staff.java
@@ -1,11 +1,29 @@
 package misc;
 
 import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import database.MySQLDatabase;
 
 public class Staff implements Serializable {
     private String name;
     private int id;
     private String password;
+    MySQLDatabase db = MySQLDatabase.getInstance();
+
+    public boolean verify(int userID, String password) {
+        ArrayList<ArrayList<String>> staff = db.getIndexValue("Staff", "id", userID);
+        int ID = Integer.parseInt(staff.get(0).get(0));
+        String pw = staff.get(0).get(1);
+        if (id == userID && pw.contentEquals(password)) {
+
+            return true;
+        }
+
+        return false;
+
+    }
 
     public int getId() {
         return id;
